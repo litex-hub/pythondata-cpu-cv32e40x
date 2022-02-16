171c9e784394ac318afa4dfbe183ce527103af75
LiteX Robot <robot@timvideos.us>
2022-02-16 09:26:07 +0000

    Updating pythondata-cpu-cv32e40x to 0.1.1.post147
    Updated data to 0.1.1-21-g34bb6aa based on 34bb6aa5261e8a1464125bfee1fef8015b3f48f4 from https://github.com/openhwgroup/cv32e40x.
    > commit 34bb6aa5261e8a1464125bfee1fef8015b3f48f4
    > Merge: 705ed19 276b3de
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Wed Feb 16 10:05:41 2022 +0100
    >
    >     Merge pull request #436 from openhwgroup/silabs-hfegran-release-notes-cfg
    >
    >     Create release.yaml (Release notes configuration)
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.1.1.post145"
-version_tuple = (0, 1, 1, 145)
+version_str = "0.1.1.post147"
+version_tuple = (0, 1, 1, 147)
 try:
     from packaging.version import Version as V
-    pversion = V("0.1.1.post145")
+    pversion = V("0.1.1.post147")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.1.1.post19"
-data_version_tuple = (0, 1, 1, 19)
+data_version_str = "0.1.1.post21"
+data_version_tuple = (0, 1, 1, 21)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.1.1.post19")
+    pdata_version = V("0.1.1.post21")
 except ImportError:
     pass
-data_git_hash = "705ed1905461e4b9e0af7d727d62008c092e8ca0"
-data_git_describe = "0.1.1-19-g705ed19"
+data_git_hash = "34bb6aa5261e8a1464125bfee1fef8015b3f48f4"
+data_git_describe = "0.1.1-21-g34bb6aa"
 data_git_msg = """\
-commit 705ed1905461e4b9e0af7d727d62008c092e8ca0
-Merge: cf8a85b e462c8d
+commit 34bb6aa5261e8a1464125bfee1fef8015b3f48f4
+Merge: 705ed19 276b3de
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Wed Feb 16 07:36:58 2022 +0100
+Date:   Wed Feb 16 10:05:41 2022 +0100
 
-    Merge pull request #435 from silabs-halfdan/mcycle_in_integration_manual
+    Merge pull request #436 from openhwgroup/silabs-hfegran-release-notes-cfg
     
-    Added missing signal in user manual integration guide
+    Create release.yaml (Release notes configuration)
 
 """
 
