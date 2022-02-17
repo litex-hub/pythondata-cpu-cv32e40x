fd8faab3196e2d466d2b15c6caa6f8f65aa7b5cb
LiteX Robot <robot@timvideos.us>
2022-02-17 15:30:11 +0000

    Updating pythondata-cpu-cv32e40x to 0.1.0.post132
    Updated data to 0.1.0-6-gf62adb3 based on f62adb348140ec9f449e9d1ffab216a5b1058e40 from https://github.com/openhwgroup/cv32e40x.
    > commit f62adb348140ec9f449e9d1ffab216a5b1058e40
    > Merge: ae8cce6 e289b7a
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Thu Feb 17 16:02:22 2022 +0100
    >
    >     Merge pull request #443 from Silabs-ArjanB/ArjanB_clku
    >
    >     Removed unused clock signal
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.1.0.post130"
-version_tuple = (0, 1, 0, 130)
+version_str = "0.1.0.post132"
+version_tuple = (0, 1, 0, 132)
 try:
     from packaging.version import Version as V
-    pversion = V("0.1.0.post130")
+    pversion = V("0.1.0.post132")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.1.0.post4"
-data_version_tuple = (0, 1, 0, 4)
+data_version_str = "0.1.0.post6"
+data_version_tuple = (0, 1, 0, 6)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.1.0.post4")
+    pdata_version = V("0.1.0.post6")
 except ImportError:
     pass
-data_git_hash = "ae8cce614fd18dcad1c880cc283943c694f0b8c7"
-data_git_describe = "0.1.0-4-gae8cce6"
+data_git_hash = "f62adb348140ec9f449e9d1ffab216a5b1058e40"
+data_git_describe = "0.1.0-6-gf62adb3"
 data_git_msg = """\
-commit ae8cce614fd18dcad1c880cc283943c694f0b8c7
-Merge: 35944de 9871699
-Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Thu Feb 17 12:15:48 2022 +0100
+commit f62adb348140ec9f449e9d1ffab216a5b1058e40
+Merge: ae8cce6 e289b7a
+Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
+Date:   Thu Feb 17 16:02:22 2022 +0100
 
-    Merge pull request #442 from Silabs-ArjanB/ArjanB_cch
+    Merge pull request #443 from Silabs-ArjanB/ArjanB_clku
     
-    Added copyright headers (not an actual documentation update)
+    Removed unused clock signal
 
 """
 
