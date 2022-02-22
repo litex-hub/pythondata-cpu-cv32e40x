00d7be29420d78d407191dd19def47d612c5da4d
LiteX Robot <robot@timvideos.us>
2022-02-22 09:28:37 +0000

    Updating pythondata-cpu-cv32e40x to 0.1.0.post145
    Updated data to 0.1.0-19-g337529c based on 337529c353ad74cda2690263252bcdbcf95c79c8 from https://github.com/openhwgroup/cv32e40x.
    > commit 337529c353ad74cda2690263252bcdbcf95c79c8
    > Merge: 45545c9 978641b
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Tue Feb 22 10:16:21 2022 +0100
    >
    >     Merge pull request #450 from silabs-oivind/align_mpu_e40s_e40x
    >
    >     Update MPU to support data access in instruction side. In preparation…
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.1.0.post143"
-version_tuple = (0, 1, 0, 143)
+version_str = "0.1.0.post145"
+version_tuple = (0, 1, 0, 145)
 try:
     from packaging.version import Version as V
-    pversion = V("0.1.0.post143")
+    pversion = V("0.1.0.post145")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.1.0.post17"
-data_version_tuple = (0, 1, 0, 17)
+data_version_str = "0.1.0.post19"
+data_version_tuple = (0, 1, 0, 19)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.1.0.post17")
+    pdata_version = V("0.1.0.post19")
 except ImportError:
     pass
-data_git_hash = "45545c989be4654d1400ebcfead53d57fadbb941"
-data_git_describe = "0.1.0-17-g45545c9"
+data_git_hash = "337529c353ad74cda2690263252bcdbcf95c79c8"
+data_git_describe = "0.1.0-19-g337529c"
 data_git_msg = """\
-commit 45545c989be4654d1400ebcfead53d57fadbb941
-Merge: 80ee57d db21f94
+commit 337529c353ad74cda2690263252bcdbcf95c79c8
+Merge: 45545c9 978641b
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Tue Feb 22 08:13:20 2022 +0100
+Date:   Tue Feb 22 10:16:21 2022 +0100
 
-    Merge pull request #449 from Silabs-ArjanB/ArjanB_obiv13
+    Merge pull request #450 from silabs-oivind/align_mpu_e40s_e40x
     
-    Updated to OBI v1.3
+    Update MPU to support data access in instruction side. In preparation…
 
 """
 
