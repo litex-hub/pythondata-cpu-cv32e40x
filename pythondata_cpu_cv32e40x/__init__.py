22f2417196377b403f81d71b9c644964fd2b270e
LiteX Robot <robot@timvideos.us>
2022-03-07 17:29:06 +0000

    Updating pythondata-cpu-cv32e40x to 0.1.0.post185
    Updated data to 0.1.0-59-g6be51b7 based on 6be51b7052888cad2f278b5fdbc9945e5b8b22cf from https://github.com/openhwgroup/cv32e40x.
    > commit 6be51b7052888cad2f278b5fdbc9945e5b8b22cf
    > Merge: a4829d1 9667651
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Mon Mar 7 17:47:54 2022 +0100
    >
    >     Merge pull request #469 from silabs-halfdan/doc_deprecated_parameter
    >
    >     Document USE_DEPRECATED_FEATURE_SET
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.1.0.post183"
-version_tuple = (0, 1, 0, 183)
+version_str = "0.1.0.post185"
+version_tuple = (0, 1, 0, 185)
 try:
     from packaging.version import Version as V
-    pversion = V("0.1.0.post183")
+    pversion = V("0.1.0.post185")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.1.0.post57"
-data_version_tuple = (0, 1, 0, 57)
+data_version_str = "0.1.0.post59"
+data_version_tuple = (0, 1, 0, 59)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.1.0.post57")
+    pdata_version = V("0.1.0.post59")
 except ImportError:
     pass
-data_git_hash = "a4829d10977941de1c1bbc73a39e420b81ec9582"
-data_git_describe = "0.1.0-57-ga4829d1"
+data_git_hash = "6be51b7052888cad2f278b5fdbc9945e5b8b22cf"
+data_git_describe = "0.1.0-59-g6be51b7"
 data_git_msg = """\
-commit a4829d10977941de1c1bbc73a39e420b81ec9582
-Merge: b4edeee d3813b0
+commit 6be51b7052888cad2f278b5fdbc9945e5b8b22cf
+Merge: a4829d1 9667651
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Fri Mar 4 14:30:04 2022 +0100
+Date:   Mon Mar 7 17:47:54 2022 +0100
 
-    Merge pull request #468 from silabs-halfdan/relative_nmi_address
+    Merge pull request #469 from silabs-halfdan/doc_deprecated_parameter
     
-    NMI address update
+    Document USE_DEPRECATED_FEATURE_SET
 
 """
 
