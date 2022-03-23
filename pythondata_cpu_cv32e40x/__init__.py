85cb02fc05de7859b2370d93f8b9a1e24d432b84
LiteX Robot <robot@timvideos.us>
2022-03-23 14:27:22 +0000

    Updating pythondata-cpu-cv32e40x to 0.2.0.post134
    Updated data to 0.2.0-8-g8738b71 based on 8738b718649a0535a83f59d6b72523282a6fb4a6 from https://github.com/openhwgroup/cv32e40x.
    > commit 8738b718649a0535a83f59d6b72523282a6fb4a6
    > Merge: 3700096 9d14b87
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Wed Mar 23 15:01:09 2022 +0100
    >
    >     Merge pull request #482 from silabs-halfdan/doc_rvfi_sleep_signals
    >
    >     Added sleep signals to rvfi documentation
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.2.0.post131"
-version_tuple = (0, 2, 0, 131)
+version_str = "0.2.0.post134"
+version_tuple = (0, 2, 0, 134)
 try:
     from packaging.version import Version as V
-    pversion = V("0.2.0.post131")
+    pversion = V("0.2.0.post134")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.2.0.post5"
-data_version_tuple = (0, 2, 0, 5)
+data_version_str = "0.2.0.post8"
+data_version_tuple = (0, 2, 0, 8)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.2.0.post5")
+    pdata_version = V("0.2.0.post8")
 except ImportError:
     pass
-data_git_hash = "37000963f8b4983ad2e35bfe3cc4d3e72977dd97"
-data_git_describe = "0.2.0-5-g3700096"
+data_git_hash = "8738b718649a0535a83f59d6b72523282a6fb4a6"
+data_git_describe = "0.2.0-8-g8738b71"
 data_git_msg = """\
-commit 37000963f8b4983ad2e35bfe3cc4d3e72977dd97
-Merge: 96f09e5 4cdfb99
-Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Wed Mar 23 10:33:12 2022 +0100
+commit 8738b718649a0535a83f59d6b72523282a6fb4a6
+Merge: 3700096 9d14b87
+Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
+Date:   Wed Mar 23 15:01:09 2022 +0100
 
-    Merge pull request #481 from Silabs-ArjanB/ArjanB_mpie
+    Merge pull request #482 from silabs-halfdan/doc_rvfi_sleep_signals
     
-    Fixed mpie R/W attribute
+    Added sleep signals to rvfi documentation
 
 """
 
