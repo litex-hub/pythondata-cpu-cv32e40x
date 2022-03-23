0d13aad4c20335ca70d6246da664f32f15559c7e
LiteX Robot <robot@timvideos.us>
2022-03-23 10:32:08 +0000

    Updating pythondata-cpu-cv32e40x to 0.2.0.post131
    Updated data to 0.2.0-5-g3700096 based on 37000963f8b4983ad2e35bfe3cc4d3e72977dd97 from https://github.com/openhwgroup/cv32e40x.
    > commit 37000963f8b4983ad2e35bfe3cc4d3e72977dd97
    > Merge: 96f09e5 4cdfb99
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Wed Mar 23 10:33:12 2022 +0100
    >
    >     Merge pull request #481 from Silabs-ArjanB/ArjanB_mpie
    >
    >     Fixed mpie R/W attribute
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.2.0.post128"
-version_tuple = (0, 2, 0, 128)
+version_str = "0.2.0.post131"
+version_tuple = (0, 2, 0, 131)
 try:
     from packaging.version import Version as V
-    pversion = V("0.2.0.post128")
+    pversion = V("0.2.0.post131")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.2.0.post2"
-data_version_tuple = (0, 2, 0, 2)
+data_version_str = "0.2.0.post5"
+data_version_tuple = (0, 2, 0, 5)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.2.0.post2")
+    pdata_version = V("0.2.0.post5")
 except ImportError:
     pass
-data_git_hash = "96f09e52e955aedf1e10df2ed6163bc09182f150"
-data_git_describe = "0.2.0-2-g96f09e5"
+data_git_hash = "37000963f8b4983ad2e35bfe3cc4d3e72977dd97"
+data_git_describe = "0.2.0-5-g3700096"
 data_git_msg = """\
-commit 96f09e52e955aedf1e10df2ed6163bc09182f150
-Merge: 902ea67 e0365b1
-Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Wed Mar 23 09:56:14 2022 +0100
+commit 37000963f8b4983ad2e35bfe3cc4d3e72977dd97
+Merge: 96f09e5 4cdfb99
+Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
+Date:   Wed Mar 23 10:33:12 2022 +0100
 
-    Merge pull request #480 from silabs-oysteink/silabs-oysteink_csr-types
+    Merge pull request #481 from Silabs-ArjanB/ArjanB_mpie
     
-    Updated typedefs for CSR registers to match new fields in the user manual
+    Fixed mpie R/W attribute
 
 """
 
