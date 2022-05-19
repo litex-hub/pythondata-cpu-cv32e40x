4d719decfb31ff4a9055d614fd95fad299e54421
LiteX Robot <robot@timvideos.us>
2022-05-19 12:19:47 +0000

    Updating pythondata-cpu-cv32e40x to 0.3.0.post243
    Updated data to 0.3.0-115-g90e95bd9 based on 90e95bd90d897ab57542066e99a382a1bb2e77d0 from https://github.com/openhwgroup/cv32e40x.
    > commit 90e95bd90d897ab57542066e99a382a1bb2e77d0
    > Merge: 22cf8949 f6225350
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Thu May 19 13:30:55 2022 +0200
    >
    >     Merge pull request #545 from Silabs-ArjanB/ArjanB_bitma
    >
    >     Fixed dependency between Zc and Zbb
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.3.0.post241"
-version_tuple = (0, 3, 0, 241)
+version_str = "0.3.0.post243"
+version_tuple = (0, 3, 0, 243)
 try:
     from packaging.version import Version as V
-    pversion = V("0.3.0.post241")
+    pversion = V("0.3.0.post243")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.3.0.post113"
-data_version_tuple = (0, 3, 0, 113)
+data_version_str = "0.3.0.post115"
+data_version_tuple = (0, 3, 0, 115)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.3.0.post113")
+    pdata_version = V("0.3.0.post115")
 except ImportError:
     pass
-data_git_hash = "22cf89493f0cfa86ad1fbf471086928da7a36e01"
-data_git_describe = "0.3.0-113-g22cf8949"
+data_git_hash = "90e95bd90d897ab57542066e99a382a1bb2e77d0"
+data_git_describe = "0.3.0-115-g90e95bd9"
 data_git_msg = """\
-commit 22cf89493f0cfa86ad1fbf471086928da7a36e01
-Merge: d7d8298e 7f79e521
+commit 90e95bd90d897ab57542066e99a382a1bb2e77d0
+Merge: 22cf8949 f6225350
 Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Thu May 19 11:50:01 2022 +0200
+Date:   Thu May 19 13:30:55 2022 +0200
 
-    Merge pull request #544 from Silabs-ArjanB/ArjanB_irqr
+    Merge pull request #545 from Silabs-ArjanB/ArjanB_bitma
     
-    Reordered content in exceptions and interrupts chapter for clarity
+    Fixed dependency between Zc and Zbb
 
 """
 
