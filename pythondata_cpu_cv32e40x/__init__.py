c2caf661f4b022d74f0ad9c2ee82a81679202772
LiteX Robot <robot@timvideos.us>
2022-05-26 16:39:15 +0000

    Updating pythondata-cpu-cv32e40x to 0.3.0.post260
    Updated data to 0.3.0-124-g285a5ad2 based on 285a5ad2baadd44a7c552c54060164c4cb6197ef from https://github.com/openhwgroup/cv32e40x.
    > commit 285a5ad2baadd44a7c552c54060164c4cb6197ef
    > Merge: ac753589 e4efbdd2
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Tue May 24 11:55:27 2022 +0200
    >
    >     Merge pull request #551 from silabs-oysteink/silabs-oysteink_wb-valid-last
    >
    >     wb_valid for all suboperations
    >
    
    Updated using 0.0.post136 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.3.0.post258"
-version_tuple = (0, 3, 0, 258)
+version_str = "0.3.0.post260"
+version_tuple = (0, 3, 0, 260)
 try:
     from packaging.version import Version as V
-    pversion = V("0.3.0.post258")
+    pversion = V("0.3.0.post260")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Tue May 24 11:55:27 2022 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post134"
-tool_version_tuple = (0, 0, 134)
+tool_version_str = "0.0.post136"
+tool_version_tuple = (0, 0, 136)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post134")
+    ptool_version = V("0.0.post136")
 except ImportError:
     pass
 
