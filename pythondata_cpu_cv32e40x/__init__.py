9b45c898fea06605488c20eead7651679ed202c7
LiteX Robot <robot@timvideos.us>
2022-05-27 07:13:07 +0000

    Updating pythondata-cpu-cv32e40x to 0.3.0.post277
    Updated data to 0.3.0-137-g3a3aecea based on 3a3aecea53f0944e6d84b0eff165740becd018a6 from https://github.com/openhwgroup/cv32e40x.
    > commit 3a3aecea53f0944e6d84b0eff165740becd018a6
    > Merge: d0e922da bdffac6d
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Fri May 27 08:53:57 2022 +0200
    >
    >     Merge pull request #552 from silabs-oysteink/silabs-oysteink_zc-tbl-1
    >
    >     Zc* table jumps
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.3.0.post264"
-version_tuple = (0, 3, 0, 264)
+version_str = "0.3.0.post277"
+version_tuple = (0, 3, 0, 277)
 try:
     from packaging.version import Version as V
-    pversion = V("0.3.0.post264")
+    pversion = V("0.3.0.post277")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.3.0.post124"
-data_version_tuple = (0, 3, 0, 124)
+data_version_str = "0.3.0.post137"
+data_version_tuple = (0, 3, 0, 137)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.3.0.post124")
+    pdata_version = V("0.3.0.post137")
 except ImportError:
     pass
-data_git_hash = "285a5ad2baadd44a7c552c54060164c4cb6197ef"
-data_git_describe = "0.3.0-124-g285a5ad2"
+data_git_hash = "3a3aecea53f0944e6d84b0eff165740becd018a6"
+data_git_describe = "0.3.0-137-g3a3aecea"
 data_git_msg = """\
-commit 285a5ad2baadd44a7c552c54060164c4cb6197ef
-Merge: ac753589 e4efbdd2
+commit 3a3aecea53f0944e6d84b0eff165740becd018a6
+Merge: d0e922da bdffac6d
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Tue May 24 11:55:27 2022 +0200
+Date:   Fri May 27 08:53:57 2022 +0200
 
-    Merge pull request #551 from silabs-oysteink/silabs-oysteink_wb-valid-last
+    Merge pull request #552 from silabs-oysteink/silabs-oysteink_zc-tbl-1
     
-    wb_valid for all suboperations
+    Zc* table jumps
 
 """
 
