05ac44206b6b9558c5bbdbd6f12ec20525e25bcc
LiteX Robot <robot@timvideos.us>
2022-05-30 06:15:58 +0000

    Updating pythondata-cpu-cv32e40x to 0.3.0.post285
    Updated data to 0.3.0-145-gf1c1500f based on f1c1500f126f6cdc5fa60c047bd46e9fd73ac1c7 from https://github.com/openhwgroup/cv32e40x.
    > commit f1c1500f126f6cdc5fa60c047bd46e9fd73ac1c7
    > Merge: c27b73db c081b107
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Mon May 30 07:41:49 2022 +0200
    >
    >     Merge pull request #557 from Silabs-ArjanB/ArjanB_ae
    >
    >     Documented Atomics extension
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.3.0.post282"
-version_tuple = (0, 3, 0, 282)
+version_str = "0.3.0.post285"
+version_tuple = (0, 3, 0, 285)
 try:
     from packaging.version import Version as V
-    pversion = V("0.3.0.post282")
+    pversion = V("0.3.0.post285")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.3.0.post142"
-data_version_tuple = (0, 3, 0, 142)
+data_version_str = "0.3.0.post145"
+data_version_tuple = (0, 3, 0, 145)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.3.0.post142")
+    pdata_version = V("0.3.0.post145")
 except ImportError:
     pass
-data_git_hash = "c27b73db52f54bab836a91d157115d85122640ec"
-data_git_describe = "0.3.0-142-gc27b73db"
+data_git_hash = "f1c1500f126f6cdc5fa60c047bd46e9fd73ac1c7"
+data_git_describe = "0.3.0-145-gf1c1500f"
 data_git_msg = """\
-commit c27b73db52f54bab836a91d157115d85122640ec
-Merge: 66b8050d 075f9a34
+commit f1c1500f126f6cdc5fa60c047bd46e9fd73ac1c7
+Merge: c27b73db c081b107
 Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
-Date:   Fri May 27 15:43:57 2022 +0200
+Date:   Mon May 30 07:41:49 2022 +0200
 
-    Merge pull request #555 from Silabs-ArjanB/ArjanB_549
+    Merge pull request #557 from Silabs-ArjanB/ArjanB_ae
     
-    Fix for issue #549. Clean up CS registers syntax. Tie RVFI to RTL ins…
+    Documented Atomics extension
 
 """
 
