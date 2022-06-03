1ea20937ba673dbe70da57520536c1e23925487d
LiteX Robot <robot@timvideos.us>
2022-06-03 09:11:36 +0000

    Updating pythondata-cpu-cv32e40x to 0.4.0.post144
    Updated data to 0.4.0-2-gcbb63d0c based on cbb63d0c475bc93ce545e0fc9e74715d80fefaa4 from https://github.com/openhwgroup/cv32e40x.
    > commit cbb63d0c475bc93ce545e0fc9e74715d80fefaa4
    > Merge: df030b15 bfa9300b
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Fri Jun 3 10:46:07 2022 +0200
    >
    >     Merge pull request #565 from silabs-oysteink/silabs-oysteink_rvfi-csr-stateen
    >
    >     Added Smstateen CSRs to RVFI (only supported in cv32e40s)
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.4.0.post142"
-version_tuple = (0, 4, 0, 142)
+version_str = "0.4.0.post144"
+version_tuple = (0, 4, 0, 144)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post142")
+    pversion = V("0.4.0.post144")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post0"
-data_version_tuple = (0, 4, 0, 0)
+data_version_str = "0.4.0.post2"
+data_version_tuple = (0, 4, 0, 2)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post0")
+    pdata_version = V("0.4.0.post2")
 except ImportError:
     pass
-data_git_hash = "df030b15727e5d3f7070bec9257bf6c539275244"
-data_git_describe = "0.4.0-0-gdf030b15"
+data_git_hash = "cbb63d0c475bc93ce545e0fc9e74715d80fefaa4"
+data_git_describe = "0.4.0-2-gcbb63d0c"
 data_git_msg = """\
-commit df030b15727e5d3f7070bec9257bf6c539275244
-Merge: 728ef777 ca2cca8f
+commit cbb63d0c475bc93ce545e0fc9e74715d80fefaa4
+Merge: df030b15 bfa9300b
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Thu Jun 2 12:38:00 2022 +0200
+Date:   Fri Jun 3 10:46:07 2022 +0200
 
-    Merge pull request #564 from Silabs-ArjanB/ArjanB_dcsrt
+    Merge pull request #565 from silabs-oysteink/silabs-oysteink_rvfi-csr-stateen
     
-    Fixed comment related to dcsr.mprven value
+    Added Smstateen CSRs to RVFI (only supported in cv32e40s)
 
 """
 
