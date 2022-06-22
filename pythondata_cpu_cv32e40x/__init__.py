fdf16bd6c105f3bb1479218a2fa0f5570b0908e8
LiteX Robot <robot@timvideos.us>
2022-06-22 18:15:43 +0000

    Updating pythondata-cpu-cv32e40x to 0.4.0.post175
    Updated data to 0.4.0-33-g967e2063 based on 967e206303a9f02d5befe49e63b8dc4ab17de233 from https://github.com/openhwgroup/cv32e40x.
    > commit 967e206303a9f02d5befe49e63b8dc4ab17de233
    > Merge: 440b5ced aa095e6c
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Wed Jun 22 19:51:56 2022 +0200
    >
    >     Merge pull request #590 from silabs-oysteink/silabs-oysteink_zc-seq-41p
    >
    >     Added missing assertion file.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.4.0.post173"
-version_tuple = (0, 4, 0, 173)
+version_str = "0.4.0.post175"
+version_tuple = (0, 4, 0, 175)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post173")
+    pversion = V("0.4.0.post175")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post31"
-data_version_tuple = (0, 4, 0, 31)
+data_version_str = "0.4.0.post33"
+data_version_tuple = (0, 4, 0, 33)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post31")
+    pdata_version = V("0.4.0.post33")
 except ImportError:
     pass
-data_git_hash = "440b5ced08e3f3029483632183904075853d2938"
-data_git_describe = "0.4.0-31-g440b5ced"
+data_git_hash = "967e206303a9f02d5befe49e63b8dc4ab17de233"
+data_git_describe = "0.4.0-33-g967e2063"
 data_git_msg = """\
-commit 440b5ced08e3f3029483632183904075853d2938
-Merge: 32c31a07 09339b1d
+commit 967e206303a9f02d5befe49e63b8dc4ab17de233
+Merge: 440b5ced aa095e6c
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Wed Jun 22 17:01:30 2022 +0200
+Date:   Wed Jun 22 19:51:56 2022 +0200
 
-    Merge pull request #588 from silabs-oysteink/silabs-oysteink_zc-seq-41p
+    Merge pull request #590 from silabs-oysteink/silabs-oysteink_zc-seq-41p
     
-    Initial implementation of Zc * sequencer
+    Added missing assertion file.
 
 """
 
