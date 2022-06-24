10774f58af988e087ca012f5ec5362cfe471230e
LiteX Robot <robot@timvideos.us>
2022-06-24 11:10:46 +0000

    Updating pythondata-cpu-cv32e40x to 0.4.0.post177
    Updated data to 0.4.0-35-g8aef8f3d based on 8aef8f3daf10271891654afdc34f0a6b58db637c from https://github.com/openhwgroup/cv32e40x.
    > commit 8aef8f3daf10271891654afdc34f0a6b58db637c
    > Merge: 967e2063 40449531
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Fri Jun 24 12:40:20 2022 +0200
    >
    >     Merge pull request #593 from Silabs-ArjanB/ArjanB_Zce1
    >
    >     Removed ZC_EXT as top level parameter. ZC_EXT is now a localparam, al…
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.4.0.post175"
-version_tuple = (0, 4, 0, 175)
+version_str = "0.4.0.post177"
+version_tuple = (0, 4, 0, 177)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post175")
+    pversion = V("0.4.0.post177")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post33"
-data_version_tuple = (0, 4, 0, 33)
+data_version_str = "0.4.0.post35"
+data_version_tuple = (0, 4, 0, 35)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post33")
+    pdata_version = V("0.4.0.post35")
 except ImportError:
     pass
-data_git_hash = "967e206303a9f02d5befe49e63b8dc4ab17de233"
-data_git_describe = "0.4.0-33-g967e2063"
+data_git_hash = "8aef8f3daf10271891654afdc34f0a6b58db637c"
+data_git_describe = "0.4.0-35-g8aef8f3d"
 data_git_msg = """\
-commit 967e206303a9f02d5befe49e63b8dc4ab17de233
-Merge: 440b5ced aa095e6c
-Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Wed Jun 22 19:51:56 2022 +0200
+commit 8aef8f3daf10271891654afdc34f0a6b58db637c
+Merge: 967e2063 40449531
+Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
+Date:   Fri Jun 24 12:40:20 2022 +0200
 
-    Merge pull request #590 from silabs-oysteink/silabs-oysteink_zc-seq-41p
+    Merge pull request #593 from Silabs-ArjanB/ArjanB_Zce1
     
-    Added missing assertion file.
+    Removed ZC_EXT as top level parameter. ZC_EXT is now a localparam, al…
 
 """
 
