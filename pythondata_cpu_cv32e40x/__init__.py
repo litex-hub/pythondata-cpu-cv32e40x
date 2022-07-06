a766ac1a7908dcf2770f1881b7882b08ba4e8750
LiteX Robot <robot@timvideos.us>
2022-07-06 09:11:51 +0000

    Updating pythondata-cpu-cv32e40x to 0.4.0.post197
    Updated data to 0.4.0-55-ge9dba5eb based on e9dba5ebb74b031712b052302c7672ca1bd45ea4 from https://github.com/openhwgroup/cv32e40x.
    > commit e9dba5ebb74b031712b052302c7672ca1bd45ea4
    > Merge: faa52330 09f3655f
    > Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
    > Date:   Wed Jul 6 11:09:29 2022 +0200
    >
    >     Merge pull request #607 from silabs-oysteink/silabs-oysteink_zc-seq-ctrl
    >
    >     Sequencer integration
    >     Self merging as agreed to enable merging to CV32E40X and continue on needed RVFI updates. Will revisit some feedback elements after the vacation season.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,34 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.4.0.post191"
-version_tuple = (0, 4, 0, 191)
+version_str = "0.4.0.post197"
+version_tuple = (0, 4, 0, 197)
 try:
     from packaging.version import Version as V
-    pversion = V("0.4.0.post191")
+    pversion = V("0.4.0.post197")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.4.0.post49"
-data_version_tuple = (0, 4, 0, 49)
+data_version_str = "0.4.0.post55"
+data_version_tuple = (0, 4, 0, 55)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.4.0.post49")
+    pdata_version = V("0.4.0.post55")
 except ImportError:
     pass
-data_git_hash = "faa52330cf49ed313f2237199cfec1f69f05ad36"
-data_git_describe = "0.4.0-49-gfaa52330"
+data_git_hash = "e9dba5ebb74b031712b052302c7672ca1bd45ea4"
+data_git_describe = "0.4.0-55-ge9dba5eb"
 data_git_msg = """\
-commit faa52330cf49ed313f2237199cfec1f69f05ad36
-Merge: 47d4a57b 3de84645
-Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Fri Jul 1 15:08:05 2022 +0200
+commit e9dba5ebb74b031712b052302c7672ca1bd45ea4
+Merge: faa52330 09f3655f
+Author: silabs-oysteink <66771756+silabs-oysteink@users.noreply.github.com>
+Date:   Wed Jul 6 11:09:29 2022 +0200
 
-    Merge pull request #606 from silabs-oysteink/silabs-oysteink_allowed-refactor
+    Merge pull request #607 from silabs-oysteink/silabs-oysteink_zc-seq-ctrl
     
-    Refactored interrupt_allowed and halt_id logic.
+    Sequencer integration
+    Self merging as agreed to enable merging to CV32E40X and continue on needed RVFI updates. Will revisit some feedback elements after the vacation season.
 
 """
 
