7c044bc942fb00170210bdbbb1ea6362bcb28f7b
LiteX Robot <robot@timvideos.us>
2022-09-29 07:34:25 +0000

    Updating pythondata-cpu-cv32e40x to 0.5.0.post186
    Updated data to 0.5.0-44-g771a63f8 based on 771a63f8583d15b7b3b95a35a0432c77cb8c08cb from https://github.com/openhwgroup/cv32e40x.
    > commit 771a63f8583d15b7b3b95a35a0432c77cb8c08cb
    > Merge: 707899d0 ee44ba7c
    > Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
    > Date:   Thu Sep 29 09:25:39 2022 +0200
    >
    >     Merge pull request #677 from silabs-oivind/rvfi_trace_performance_fix
    >
    >     Fix performance issue in rvfi_sim_trace and enable it by default.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_cv32e40x/__init__.py
+++ pythondata_cpu_cv32e40x/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/openhwgroup/cv32e40x"
 
 # Module version
-version_str = "0.5.0.post184"
-version_tuple = (0, 5, 0, 184)
+version_str = "0.5.0.post186"
+version_tuple = (0, 5, 0, 186)
 try:
     from packaging.version import Version as V
-    pversion = V("0.5.0.post184")
+    pversion = V("0.5.0.post186")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.5.0.post42"
-data_version_tuple = (0, 5, 0, 42)
+data_version_str = "0.5.0.post44"
+data_version_tuple = (0, 5, 0, 44)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.5.0.post42")
+    pdata_version = V("0.5.0.post44")
 except ImportError:
     pass
-data_git_hash = "707899d0ee31c6ae904c984dac3fa4a029e472b6"
-data_git_describe = "0.5.0-42-g707899d0"
+data_git_hash = "771a63f8583d15b7b3b95a35a0432c77cb8c08cb"
+data_git_describe = "0.5.0-44-g771a63f8"
 data_git_msg = """\
-commit 707899d0ee31c6ae904c984dac3fa4a029e472b6
-Merge: 9b33f911 4f7bd710
+commit 771a63f8583d15b7b3b95a35a0432c77cb8c08cb
+Merge: 707899d0 ee44ba7c
 Author: Arjan Bink <40633348+Silabs-ArjanB@users.noreply.github.com>
-Date:   Wed Sep 28 11:48:44 2022 +0200
+Date:   Thu Sep 29 09:25:39 2022 +0200
 
-    Merge pull request #669 from silabs-oysteink/silabs_oysteink_wfe
+    Merge pull request #677 from silabs-oivind/rvfi_trace_performance_fix
     
-    Implemented custom WFE instruction
+    Fix performance issue in rvfi_sim_trace and enable it by default.
 
 """
 
